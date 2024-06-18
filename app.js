b5638a3bbb55154aa82b4542932e6b96acb6f63e
meeraparigi <meera22_99@yahoo.com>
2024-06-18 17:28:43 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to the World of Cloud!'));
+app.get('/', (req, res) => res.send('We have added a Review Stage with manual approvel to our Code Pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
